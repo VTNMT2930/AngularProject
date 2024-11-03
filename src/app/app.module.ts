e782f0c2e60d0e1074ec293414bfad43849f874b
VTNMT2930 <nhantrung297@gmail.com>
2024-11-04 00:42:03 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,31 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { IntroduceComponent } from './components/introduce/introduce.component';
+import { ProductComponent } from './components/product/product.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IntroduceComponent,
+    ProductComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
