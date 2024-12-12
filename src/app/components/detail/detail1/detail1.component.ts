d2d467a593effe888f053b3d46fa2db2f384f0a2
VTNMT2930 <nhantrung297@gmail.com>
2024-12-12 10:12:28 +0700

    Fix

--- src/app/components/detail/detail1/detail1.component.ts
+++ src/app/components/detail/detail1/detail1.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../models/product.model';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-detail1',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './detail1.component.css'
 })
 export class Detail1Component {
+  product: Product = {
+    id: 1,
+    name: 'MLB Women\'s Denim',
+    price: 2300000,
+    image: 'assets/Local/dress8.webp',
+    quantity: 1
+  };
 
+  quantity: number = 1;
+
+  constructor(private cartService: CartService) {}
+
+  addToCart() {
+    this.cartService.addToCart({...this.product, quantity: this.quantity });
+  }
 }
